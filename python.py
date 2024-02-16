11159d95038c40f451435a4ce594f6e0da4060b4
Amit Adhikary <amitadhikary59684@gmail.com>
2024-02-16 14:44:45 +0530

    Load Words Function

--- python.py
+++ python.py
@@ -1,3 +1,18 @@
+def load_words_from_json(category):
+    try:
+        with open(WORD_LIST_FILE, 'r') as file:
+            word_list = json.load(file)
+            words = word_list.get(category, [])
+            print(f"Loaded words for '{category}': {words}")
+            return words
+    except FileNotFoundError:
+        print("Word list file not found.")
+        sys.exit()
+    except json.decoder.JSONDecodeError:
+        print("Error decoding JSON in the word list file.")
+        sys.exit()
+
+
 def get_user_input():
     return input("Type the words exactly as shown. Press 'Ctrl + Q' to quit.\n")
 
